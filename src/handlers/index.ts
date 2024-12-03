b59f30ba99b172d57d1ec8724bc196ce1c5341a0
jesus <jabakabaka@gmail.com>
2024-12-02 19:42:25 -0600

    Feature: autenticacion y middlerware

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,17 +1,20 @@
 import {Request, Response} from 'express';
 import User from "../models/User";
-import {hashPassword} from "../utils/auth";
+import {checkPassword, hashPassword} from "../utils/auth";
+import { validationResult } from "express-validator";
+import slug from "slug";
 
-export const createAccount = async (req: Request, res: Response) => {
+export const createAccount = async (req: Request, res) => {
+
     const { email, password } = req.body;
     // handle edit
-    const slug = (await import('slug')).default;
 
     const handle = slug(req.body.handle, '')
     const handleExists = await User.findOne({ handle });
     if (handleExists) {
         const error = new Error('Handle already exists')
-        return res.status(409).json({ error : error.message })
+        res.status(409).json({ error : error.message })
+        return
     }
     const userExists = await User.findOne({ email });
     if (!userExists) {
@@ -19,11 +22,30 @@ export const createAccount = async (req: Request, res: Response) => {
         user.password = await hashPassword(password)
         user.handle = handle
         await user.save()
-        res.status(200).json({success: true, message: 'Email already exists'})
+        res.status(200).json({success: true, message: 'Account created successfully'})
     } else {
-        const error = new Error('User already exists')
+        const error = new Error('Email already exists')
         return res.status(409).json({ error : error.message })
     }
 
+}
 
+export const login = async (req: Request, res: Response) => {
+
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({errors : errors.array()});
+
+    }
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        const error = new Error('User not exist')
+        res.status(404).json({errors : error.message});
+    }
+    const isPasswordCorrect = await checkPassword(password, user.password);
+    if (!isPasswordCorrect) {
+        const error = new Error('Password incorrect')
+        res.status(401).json({errors : error.message});
+    }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import colors from "colors";
-import {createAccount} from "./handlers";
+import {createAccount, login} from "./handlers";
+import { body } from "express-validator";
+import {handleInputErrors} from "./middleware/validation";
 
 const router = Router();
 
@@ -11,6 +13,29 @@ router.get('/hola', (req, res) => {
 });
 
 // @ts-ignore
-router.post('/auth/register', createAccount);
+router.post('/auth/register',
+    body('handle')
+        .notEmpty()
+        .withMessage('Handle not exist'),
+    body('name')
+        .notEmpty()
+        .withMessage('Name not exist'),
+    body('email')
+        .isEmail()
+        .withMessage('Email not valid'),
+    body('password')
+        .isLength({min: 8})
+        .withMessage('Password too short, minimun 8 characters'),
+    handleInputErrors,
+    createAccount);
+
+router.post('/auth/login',
+    body('email')
+        .isEmail()
+        .withMessage('Email not valid'),
+    body('password')
+        .notEmpty()
+        .withMessage('Password required'),
+    login);
 
 export default router   ;
