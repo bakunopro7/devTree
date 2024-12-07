22ee9628cf78f25630ee47807a6fb33d41297b51
jesus <jabakabaka@gmail.com>
2024-12-07 15:20:37 -0600

    Feature: add cors configration

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,16 @@ import express from 'express';
 import 'dotenv/config'
 import router from './router';
 import {connectDB} from "./config/db";
+import cors from 'cors';
+import {corsConfig} from "./config/cors";
 
 const app = express();
 connectDB()
 
 // Middleware para parsear JSON
 app.use(express.json());
-
+// Cors
+app.use(cors(corsConfig));
 // use principal function to entry points
 app.use('/', router );
 
